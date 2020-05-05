20a009bca880b91cd9ed8d7d7ed413220e403056
yters <eric.holloway@gmail.com>
2020-05-04 20:02:02 -0400

    more animals

--- cluster.py
+++ cluster.py
@@ -4,8 +4,9 @@ from itertools import combinations
 def nearest_neighbor_cluster(triplets):
     index = {}
     neighbs = {}
-    for s, a, b in sorted(triplets, reverse=False):
-        if s > float(sys.argv[2]): break 
+    for s, a, b in sorted(triplets, reverse=(sys.argv[4] == 'True')):
+        if s > float(sys.argv[2]) and not sys.argv[4] == 'True': break 
+        if s < float(sys.argv[2]) and sys.argv[4] == 'True': break 
         if neighbs.get(a, 0) < int(sys.argv[3]): 
             neighbs[a] = neighbs.get(a, 0) + 1
             cluster = index.get(a, set())
